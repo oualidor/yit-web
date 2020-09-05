928e82a895097b4a575b9317b0fd02edab91c398
oualid KHIAL <walid.khial@gmail.com>
2020-09-05 21:32:05 +0100

    Radival changes, forget to commit :p

---
+++ src/Components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../Style/Components/Footer.css"
+import NavBar from "./NavBar";
+import Logo from "./Logo";
+
+class Footer extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <React.Fragment>
+                <div className={"Footer"}>
+
+                </div>
+            </React.Fragment>
+
+        )
+    }
+}
+
+
+export default Footer
---
+++ src/Components/SquareAnimation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../Style/Components/SquareAnimation.css"
+import $ from 'jquery';
+
+class SquareAnimation extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+
+
+    }
+    render(){
+        return(
+            <React.Fragment>
+                    <ul className={"SquareAnimation"}>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+            </React.Fragment>
+
+        )
+    }
+}
+
+
+export default SquareAnimation
